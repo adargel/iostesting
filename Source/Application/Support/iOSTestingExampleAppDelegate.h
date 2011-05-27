cd3d5821ea2a7c6c8810e8f8ce29fee72d9cb797
Aaron Dargel <aaron@nervousyak.com>
2011-05-27 16:21:30 -0500

    Created the project structure for the testing example project.

---
+++ Source/Application/Support/iOSTestingExampleAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  iOSTestingExampleAppDelegate.h
+//  iOSTestingExample
+//
+//  Created by Aaron Dargel on 5/27/11.
+//  Copyright 2011 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iOSTestingExampleAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
